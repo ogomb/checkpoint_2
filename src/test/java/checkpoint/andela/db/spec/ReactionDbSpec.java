773feee5a2b907978811d3276a748503d4a1630f
ogomb <mbogolewis@gmail.com>
2018-03-29 11:56:55 +0300

    test creation of a collection.

--- src/test/java/checkpoint/andela/db/spec/ReactionDbSpec.java
+++ src/test/java/checkpoint/andela/db/spec/ReactionDbSpec.java
@@ -1,14 +1,28 @@
 package checkpoint.andela.db.spec;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.net.UnknownHostException;
+
 import static org.junit.Assert.*;
 
 public class ReactionDbSpec {
+    ReactionDb collection;
+
+    @Before
+    public void before() throws UnknownHostException {
+        collection = new ReactionDb();
+    }
 
     @Test
     public void whenInstantiatedThenMongoHasDBNameReactionDb(){
-        ReactionDb collection = new ReactionDb();
         assertEquals("ReactionDb", collection.getMongoCollection()
                 .getDBCollection().getDB().getNme());
+    }
+
+    @Test
+    public void whenInstantiatedThenMongoCollectionHasNameReactions(){
+        assertEquals("reactions", collection.getMongoCollection().getName());
     }
 }
