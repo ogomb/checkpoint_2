92b5a928cb9443744f01d5b7476a515512569ed1
ogomb <mbogolewis@gmail.com>
2018-03-29 12:45:32 +0300

    saadd save method for the database.

--- src/main/java/checkpoint/andela/db/ReactionDb.java
+++ src/main/java/checkpoint/andela/db/ReactionDb.java
@@ -8,13 +8,18 @@ import org.jongo.MongoCollection;
 import java.net.UnknownHostException;
 
 public class ReactionDb {
-    private MongoCollection mongoCollection;
-    protected  MongoCollection getMongoCollection(){
-        return mongoCollection;
-    }
+        private MongoCollection mongoCollection;
+        protected  MongoCollection getMongoCollection(){
+            return mongoCollection;
+        }
 
-    public ReactionDb() throws UnknownHostException {
-        DB database = new MongoClient().getDB("ReactionDb");
-        mongoCollection = new Jongo(database).getCollection("reactions");
-    }
+        public ReactionDb() throws UnknownHostException {
+            DB database = new MongoClient().getDB("ReactionDb");
+            mongoCollection = new Jongo(database).getCollection("reactions");
+        }
+
+        public void saveMode(FileToSave bean){
+
+        }
 }
+
