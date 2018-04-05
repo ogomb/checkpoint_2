80b725c60018484ecbe3214fbfb065d340f565a8
ogomb <mbogolewis@gmail.com>
2018-04-05 13:34:40 +0300

    error should be thrown when collection is not passed.

--- src/test/java/checkpoint/andela/db/DBWriterSpec.java
+++ src/test/java/checkpoint/andela/db/DBWriterSpec.java
@@ -2,13 +2,16 @@ package checkpoint.andela.db;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.net.UnknownHostException;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class DBWriterSpec {
 
     @Before
-    public final void before(){
+    public final void before() throws UnknownHostException{
         collection = mock(ReactionDb.class);
         dBWriter = new DBWriter();
     }
