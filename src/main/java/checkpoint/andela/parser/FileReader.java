1a53257af6a1d6d478cee4290e77f8cc284414ec
ogomb <mbogolewis@gmail.com>
2018-04-06 14:57:58 +0300

    create a method to act as a producer of lines of code.

--- src/main/java/checkpoint/andela/parser/FileReader.java
+++ src/main/java/checkpoint/andela/parser/FileReader.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class FileReader {
+    FileParser  parser = FileParser.getInstance();
     public static void main(String[] args) throws UnknownHostException {
         Thread thread1 = new Thread(new Runnable() {
             @Override
@@ -27,21 +28,12 @@ public class FileReader {
 
         DBWriter writer = new DBWriter();
         File file = new File("reactions.dat");
-        FileParser  parser = FileParser.getInstance();
+
 
         try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
             String line;
             while ((line = br.readLine()) != null){
-                parser.uniqueId(line)
-                        .getTypes(line)
-                        .getInPathWay(line)
-                        .getCredits(line)
-                        .getECNumbers(line)
-                        .getEnzymaticReaction(line)
-                        .getCommonName(line)
-                        .getRight(line)
-                        .getLeft(line)
-                        .getSystematicName(line);
+
                 if (line.startsWith("//")) {
                     writer.saveRecord(parser.getMap());
                     parser.clearSet();
@@ -58,4 +50,19 @@ public class FileReader {
         }
 
     }
+
+    public void producer(String line){
+        parser.uniqueId(line)
+                .getTypes(line)
+                .getInPathWay(line)
+                .getCredits(line)
+                .getECNumbers(line)
+                .getEnzymaticReaction(line)
+                .getCommonName(line)
+                .getRight(line)
+                .getLeft(line)
+                .getSystematicName(line);
+    }
+
+
 }
