63750a443bc4b87b3eee83eefa819f53b5950f8e
ogomb <mbogolewis@gmail.com>
2018-04-07 16:01:40 +0300

    add threads for writting and reading a file.

--- src/main/java/checkpoint/andela/parser/FileReader.java
+++ src/main/java/checkpoint/andela/parser/FileReader.java
@@ -9,70 +9,62 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 public class FileReader {
-    FileParser  parser = FileParser.getInstance();
-    DBWriter writer;
-    {
-        try {
-            writer = new DBWriter();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws UnknownHostException {
+        FileParser  parser = FileParser.getInstance();
+        DBWriter writer = new DBWriter();
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
+                File file = new File("reactions.dat");
+                try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
+                    String line;
+                    while ((line = br.readLine()) != null){
+                        System.out.println(Thread.currentThread().getName());
+                        parser.uniqueId(line)
+                                .getTypes(line)
+                                .getInPathWay(line)
+                                .getCredits(line)
+                                .getECNumbers(line)
+                                .getEnzymaticReaction(line)
+                                .getCommonName(line)
+                                .getRight(line)
+                                .getLeft(line)
+                                .getSystematicName(line);
 
-            }
-        });
+                        if (line.startsWith("//")) {
+                            Thread thread2 = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    System.out.println(Thread.currentThread().getName());
+                                    writer.saveRecord(parser.getMap());
+                                    parser.clearSet();
+                                }
+                            });
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
-
-
-
-        File file = new File("reactions.dat");
-
-
-        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){
-            String line;
-            while ((line = br.readLine()) != null){
-
-                if (line.startsWith("//")) {
-
+                            thread2.start();
+                            try {
+                                thread2.join();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        continue;
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("Can't find file");
+                } catch (IOException e) {
+                    System.out.println("Unable to read file");
                 }
-                continue;
-
             }
+        });
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Can't find file");
-        } catch (IOException e) {
-            System.out.println("Unable to read file");
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
-
-    public void producer(String line){
-        parser.uniqueId(line)
-                .getTypes(line)
-                .getInPathWay(line)
-                .getCredits(line)
-                .getECNumbers(line)
-                .getEnzymaticReaction(line)
-                .getCommonName(line)
-                .getRight(line)
-                .getLeft(line)
-                .getSystematicName(line);
-    }
-
-    public void consumer(){
-        writer.saveRecord(parser.getMap());
-        parser.clearSet();
-    }
 }
+
