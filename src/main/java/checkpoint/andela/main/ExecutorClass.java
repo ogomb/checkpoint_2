a1324309d90ee80dcd158ef5db58bb5929052b78
ogomb <mbogolewis@gmail.com>
2018-04-07 16:21:16 +0300

    thread2 for writting to the database.

--- src/main/java/checkpoint/andela/main/ExecutorClass.java
+++ src/main/java/checkpoint/andela/main/ExecutorClass.java
@@ -1,11 +1,17 @@
 package checkpoint.andela.main;
 
+import checkpoint.andela.db.DBWriter;
+import checkpoint.andela.parser.FileParser;
 import checkpoint.andela.parser.FileReader;
+
+import java.net.UnknownHostException;
 
 public class ExecutorClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         FileReader  fileReader = new FileReader();
+        DBWriter writer = new DBWriter();
+        FileParser parser = FileParser.getInstance();
 
         Thread thread1 = new Thread(new Runnable() {
             @Override
@@ -16,6 +22,17 @@ public class ExecutorClass {
                     e.printStackTrace();
                 }
 
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    writer.consumer(parser);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
