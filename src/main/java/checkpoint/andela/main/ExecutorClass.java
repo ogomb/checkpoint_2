84e33d9dcafcac163373a31e6661ab4a8ec5a7c3
ogomb <mbogolewis@gmail.com>
2018-04-07 16:18:47 +0300

    thread1 for reading from a file.

--- src/main/java/checkpoint/andela/main/ExecutorClass.java
+++ src/main/java/checkpoint/andela/main/ExecutorClass.java
@@ -1,8 +1,23 @@
 package checkpoint.andela.main;
 
+import checkpoint.andela.parser.FileReader;
+
 public class ExecutorClass {
 
     public static void main(String[] args) {
+        FileReader  fileReader = new FileReader();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    fileReader.producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
     }
 }
