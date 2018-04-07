88027de44b87b18c8e668ce1ddd980c77ea40d15
ogomb <mbogolewis@gmail.com>
2018-04-07 16:22:11 +0300

    add method to start threads.

--- src/main/java/checkpoint/andela/main/ExecutorClass.java
+++ src/main/java/checkpoint/andela/main/ExecutorClass.java
@@ -36,5 +36,15 @@ public class ExecutorClass {
             }
         });
 
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
